7052604b468f2132ab7562f2b0bfb29708a48d3b
kathb <kzb1895@gmail.com>
2015-09-22 13:32:19 -0600

    Added useBuzzer and testReflex activities

--- app/src/main/java/com/ualberta/kmbaker/kmbaker_reflex/testReflexActivity.java
+++ app/src/main/java/com/ualberta/kmbaker/kmbaker_reflex/testReflexActivity.java
@@ -5,20 +5,20 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 
-public class MainActivity extends ActionBarActivity {
+public class testReflexActivity extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_test_reflex);
+        Intent intent = getIntent();//need this??
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        getMenuInflater().inflate(R.menu.menu_test_reflex, menu);
         return true;
     }
 
@@ -35,18 +35,5 @@ public class MainActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
-    }
-    /*Respond to Single User button click
-    * Must be public void and have a View as the only parameter*/
-    public void testReflex(View view) {
-        /*Do something - go to test reflex*/
-        Intent intent = new Intent(this, testReflexActivity.class);
-        startActivity(intent);
-    }
-
-    public void useBuzzer(View view) {
-        /*Go to gameshow buzzer activity*/
-        Intent intent = new Intent(this, useBuzzerActivity.class);
-        startActivity(intent);
     }
 }
--- app/src/main/java/com/ualberta/kmbaker/kmbaker_reflex/useBuzzerActivity.java
+++ app/src/main/java/com/ualberta/kmbaker/kmbaker_reflex/useBuzzerActivity.java
@@ -1,24 +1,22 @@
 package com.ualberta.kmbaker.kmbaker_reflex;
 
-import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 
-public class MainActivity extends ActionBarActivity {
+public class useBuzzerActivity extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_use_buzzer);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        getMenuInflater().inflate(R.menu.menu_use_buzzer, menu);
         return true;
     }
 
@@ -35,18 +33,5 @@ public class MainActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
-    }
-    /*Respond to Single User button click
-    * Must be public void and have a View as the only parameter*/
-    public void testReflex(View view) {
-        /*Do something - go to test reflex*/
-        Intent intent = new Intent(this, testReflexActivity.class);
-        startActivity(intent);
-    }
-
-    public void useBuzzer(View view) {
-        /*Go to gameshow buzzer activity*/
-        Intent intent = new Intent(this, useBuzzerActivity.class);
-        startActivity(intent);
     }
 }
